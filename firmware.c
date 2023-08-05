84106c1cad7bcc66d98d36449490ff004b8f530a
Ryan McClue <re.mcclue@protonmail.com>
2023-08-05 13:49:47 +1000

    j

--- firmware.c
+++ firmware.c
@@ -9,9 +9,13 @@
 //    - have one configuration for flashing/stopping and other for attaching
 
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/systick.h>
 
 #define LED_PORT (GPIOA)
 #define LED_PIN (GPIO5)
+
+// referred to as 'advanced' bus as highly configurable (speed and transfer) and connects different peripherals 
 
 static void
 rcc_setup(void)
@@ -22,6 +26,21 @@ rcc_setup(void)
   rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
 }
 
+// systick peripheral is wall clock
+static void
+systick_setup(void)
+{
+  // 84MHz gives 84cycles/ns. However, rarely operate at this granularity in code; would be in a peripheral
+  // us common, ms often
+  // 1000 ticks per second, so ms
+  systick_set_frequency(1000, 84000000);
+  systick_counter_enable();
+  // lets us know via maskable interrupt
+  systick_interrupt_enable();
+
+  // an exception like TRAP is synchronous in nature
+}
+
 static void
 gpio_setup(void)
 {
@@ -29,9 +48,12 @@ gpio_setup(void)
   // effectively 'turn them on' by enabling clock signal
   rcc_periph_clock_enable(RCC_GPIOA);
 
-  // as hardware involved, the atomicity provided by ORing pins is good
+  // the ports (ORing pins) provide atomicity, e.g. can read/write to 16pins simultaneously
   gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
 }
+
+// #define MMIO_32(addr) (*(volatile u32 *)addr)
+// memory mapped address hierarchy typically 'bus + peripheral + register' 
 
 // 4.2billion enough
 static void
@@ -43,6 +65,9 @@ delay_cycles(u32 cycles)
   }
 }
 
+
+// IMPORTANT: web docs with hyperlinks are not present for embedded. finding information can be more involved
+
 // IMPORTANT: recognise that the datasheet is written for diverse group of people with different requirements
 // e.g, if firmware engineer working on dev-board, not concerned with hardware engineer building board CPU-die package size, electrical characteristics for power supply etc.
 // more concerned with peripheral capabilities
@@ -51,6 +76,7 @@ delay_cycles(u32 cycles)
 // ARM is owns intellectual property
 // cortex-m4 TRM useful to have so can know what is common across all chips, and what is specific to yours 
 // also cortex-m4 programming manual
+
 
 int
 main(void)
